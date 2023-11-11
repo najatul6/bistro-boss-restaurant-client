a8ffa897b6393ff3e0244756ce7305e6a185c898
najatul6 <mdnajatulislam.develop@gmail.com>
2023-11-12 00:12:15 +0600

    Set up restaurant client side and install react router, tailwind, firebase  and daisy

---
+++ src/Config/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,10 @@
+
+const MainLayout = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default MainLayout;
